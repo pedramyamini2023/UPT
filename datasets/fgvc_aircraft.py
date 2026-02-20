40f6b3d85acf2e2d418c07b65c6a761f8839c9f9
Pedram Yamini <pedramyamini92@gmail.com>
2026-02-20 15:04:37 +0330

    changing fgvc_aircraft.py path

--- datasets/fgvc_aircraft.py
+++ datasets/fgvc_aircraft.py
@@ -20,7 +20,7 @@ class FGVCAircraft(DatasetBase):
         mkdir_if_missing(self.split_fewshot_dir)
 
         classnames = []
-        with open(os.path.join(self.dataset_dir, "variants.txt"), "r") as f:
+        with open(os.path.join(self.dataset_dir,"data", "variants.txt"), "r") as f:
             lines = f.readlines()
             for line in lines:
                 classnames.append(line.strip())
